4f9ef18a8207ba33ab745ee56f6721c68285609b
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-12-31 22:09:13 +0530

    update shipping address table

--- HandWoven.Api/Models/ShippingAddress.cs
+++ HandWoven.Api/Models/ShippingAddress.cs
@@ -1,8 +1,29 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HandWoven.Api.Models;
 
 public class ShippingAddress
 {
+    [Key]
+    public int AddressId { get; set; }
 
+    public int UserId { get; set; }
+    public User User { get; set; } = null!;
+
+    [Required]
+    public string AddressLine1 { get; set; } = null!;
+    public string? AddressLine2 { get; set; }
+
+    [Required]
+    public string City { get; set; } = null!;
+    public string? State { get; set; }
+
+    [Required]
+    public string Country { get; set; } = null!;
+
+    [Required]
+    public string PostalCode { get; set; } = null!;
+
+    public string? AdditionalPhoneNo { get; set; }
 }
