5b229f6e708ca5f45fa222724b2df9d6845105f0
Prasad Manamperi <prasadmanamperi130@gmail.com>
2026-01-10 23:23:49 +0530

    define foreign keys

--- HandWoven.Api/Data/AppDbContext.cs
+++ HandWoven.Api/Data/AppDbContext.cs
@@ -83,7 +83,7 @@ public class AppDbContext : DbContext
             .HasForeignKey(r => r.ShopId)
             .OnDelete(DeleteBehavior.Cascade);
 
-        
+
         // Wishlist
         modelBuilder.Entity<Wishlist>()
             .HasIndex(w => new { w.UserId, w.ProductId })
@@ -101,6 +101,73 @@ public class AppDbContext : DbContext
             .HasForeignKey(w => w.ProductId)
             .OnDelete(DeleteBehavior.Cascade);
 
+
+        // Cart
+        modelBuilder.Entity<Cart>()
+            .HasIndex(c => new { c.UserId, c.ProductId })
+            .IsUnique();
+
+        modelBuilder.Entity<Cart>()
+            .HasOne(c => c.User) // each cart has one user
+            .WithMany(u => u.Carts) // each user can have many carts
+            .HasForeignKey(c => c.UserId) // the foreignkey is UserId in the cart table
+            .OnDelete(DeleteBehavior.Cascade); // if a user is deleted, all their cart is deleted too
+
+        modelBuilder.Entity<Cart>()
+            .HasOne(c => c.Product)
+            .WithMany()
+            .HasForeignKey(c => c.ProductId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+
+
+        // Shipping address
+        modelBuilder.Entity<ShippingAddress>()
+            .HasOne(sa => sa.User)
+            .WithMany(u => u.ShippingAddresses)
+            .HasForeignKey(sa => sa.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+
+        // Order
+        modelBuilder.Entity<Order>()
+            .HasOne(o => o.User)
+            .WithMany(u => u.Orders)
+            .HasForeignKey(o => o.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Order>()
+            .HasOne(o => o.ShippingAddress)
+            .WithMany()
+            .HasForeignKey(o => o.AddressId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // Order Item
+        modelBuilder.Entity<OrderItem>()
+            .HasOne(oi => oi.Order)
+            .WithMany(o => o.OrderItems)
+            .HasForeignKey(oi => oi.OrderId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<OrderItem>()
+            .HasOne(oi => oi.Product)
+            .WithMany()
+            .HasForeignKey(oi => oi.ProductId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<OrderItem>()
+            .HasOne(oi => oi.Seller)
+            .WithMany()
+            .HasForeignKey(oi => oi.SellerId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+
+        // Promotion
+        modelBuilder.Entity<Promotion>()
+            .HasOne(p => p.Product)
+            .WithMany(pr => pr.Promotions)
+            .HasForeignKey(p => p.ProductId)
+            .OnDelete(DeleteBehavior.Cascade);
 
     }
 }
