fa1ccefbfd1a25473dfee1cf88323f9009ff618e
Prasad Manamperi <prasadmanamperi130@gmail.com>
2026-01-15 11:52:27 +0530

    fix: semicolon error

--- HandWoven.Api/DTOs/Auth/AuthResponseDto.cs
+++ HandWoven.Api/DTOs/Auth/AuthResponseDto.cs
@@ -1,7 +1,7 @@
 public class AuthResponseDto
 {
     public string Token { get; set; } = null!;
-    public DateTime ExpiresAt { get; set; };
+    public DateTime ExpiresAt { get; set; }
 
     public int UserId {get; set;}
     public string Email {get; set;} = null!;
