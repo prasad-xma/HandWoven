2bc9b8b93314ac3c0e49b4fb4ec065285a639bdc
Prasad Manamperi <prasadmanamperi130@gmail.com>
2026-01-16 14:14:07 +0530

    update: add authentication and authorization

--- HandWoven.Api/Program.cs
+++ HandWoven.Api/Program.cs
@@ -1,24 +1,77 @@
 using Microsoft.EntityFrameworkCore;
 using HandWoven.Api.Data;
+using HandWoven.Api.Configurations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using HandWoven.Api.Services.Auth;
+using HandWoven.Api.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// controllers
-builder.Services.AddControllers();
-
-// Swagger (API Testing)
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 // Database MySQL and Pomelo
-builder.Services.AddDbContext<AppDbContext>(options => 
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
     options.UseMySql(
         builder.Configuration.GetConnectionString("DefaultConnection"),
         new MySqlServerVersion(new Version(8, 0, 36))
-    )
+    );
+});
+
+// controllers ------------------------------
+builder.Services.AddControllers();
+
+
+// JWT Settings ----------------------------
+builder.Services.Configure<JwtSettings>(
+    builder.Configuration.GetSection("JwtSettings")
 );
 
-// CROS for react frontend
+// Authentication ---------------------------
+var jwtSettings = builder.Configuration.GetSection("JwtSettings")
+    .Get<JwtSettings>();
+
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(option =>
+{
+    option.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = jwtSettings!.Issuer,
+        ValidAudience = jwtSettings.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(jwtSettings.key)
+        )
+    };
+});
+
+// Authorization ------------------------------------------
+builder.Services.AddAuthorization();
+
+
+
+// Services ------------------------------------------------
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<JwtTokenGenerator>();
+
+
+
+// Swagger (API Testing) ---------------------------------
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+// CROS for react frontend --------------------------------------
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
@@ -44,6 +97,9 @@ app.UseHttpsRedirection();
 
 app.UseCors("AllowAll");
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
