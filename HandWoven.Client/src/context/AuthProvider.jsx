5bbf5d609f46047d42df3f5d7dfc512d112ed8ae
Prasad Manamperi <prasadmanamperi130@gmail.com>
2026-01-18 12:26:53 +0530

    update auth provider with token decode

--- HandWoven.Client/src/context/AuthProvider.jsx
+++ HandWoven.Client/src/context/AuthProvider.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import { AuthContext } from './AuthContext';
+import jwtDecode from "jwt-decode";
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(() => {
+        const token = localStorage.getItem("token");
+        return token ? jwtDecode(token) : null;
+    });
 
-    const loginUser = (token, userData) => {
+    const loginUser = (token) => {
         localStorage.setItem("token", token);
-        setUser(userData);
+        const decoded = jwtDecode(token);
+        setUser(decoded);
     };
 
     const logout = () => {
