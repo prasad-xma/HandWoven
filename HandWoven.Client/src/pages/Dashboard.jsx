bb2375e2d9b0804c205603aeede939d9f7da86b2
Prasad Manamperi <prasadmanamperi130@gmail.com>
2026-01-20 19:49:23 +0530

    add potal to navigate to the seller account page

--- HandWoven.Client/src/pages/Dashboard.jsx
+++ HandWoven.Client/src/pages/Dashboard.jsx
@@ -1,18 +1,27 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
   const { user, logout } = useContext(AuthContext);
-  // console.log(user);
+  const navigate = useNavigate();
+  
+
   return (
-    <>
+    <div>
       <h2>Dashboard</h2>
       <p>Email: {user?.email}</p>
       <p>Role: {user?.role}</p>
       {/* Role: {user?.["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"]} */}
-      <button onClick={logout}>Logout</button>
-    </>
-  )
+      <button className='mt-2 cursor-pointer' onClick={logout}>Logout</button>
+      <br />
+      {user.role === "User" && (
+        <button className='mt-2 cursor-pointer' onClick={() => navigate("/seller/register")}>
+          Create Seller Account
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default Dashboard;
