a6fc5e4639a0e96e0a5ef4fb73fc2e866ad95c4f
Prasad Manamperi <prasadmanamperi130@gmail.com>
2026-01-24 19:23:30 +0530

    create cart APIs

---
+++ HandWoven.Client/src/api/cartApi.js
@@ -0,0 +1,26 @@
+import api from './axios';
+
+export const getCart = async () => {
+    const response = await api.get('/cart');
+    return response.data;
+};
+
+export const addToCart = async (productId, quantity) => {
+    const response = await api.post('/cart/add', { productId, quantity });
+    return response.data;
+};
+
+export const updateCartItem = async (cartId, quantity) => {
+    const response = await api.put(`/cart/${cartId}`, { quantity });
+    return response.data;
+};
+
+export const removeFromCart = async (cartId) => {
+    const response = await api.delete(`/cart/${cartId}`);
+    return response.data;
+};
+
+export const clearCart = async () => {
+    const response = await api.delete('/cart/clear');
+    return response.data;
+};
