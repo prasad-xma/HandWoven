897698b1c5ecc633b808e6e6471dcc0c66762343
Prasad Manamperi <prasadmanamperi130@gmail.com>
2026-01-25 11:44:04 +0530

    create stripe settings

---
+++ HandWoven.Api/Configurations/StripeSettings.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace HandWoven.Api.Configurations;
+
+public class StripeSettings
+{
+    public string? SecretKey { get; set; }
+
+    public string? Publickey { get; set; }
+}
