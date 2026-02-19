e8d518d801b13c03dd870eb808e6ff7ef8bb0b63
Prasad Manamperi <prasadmanamperi130@gmail.com>
2026-02-19 10:44:56 +0530

    update: comment for testing

--- HandWoven.Api/Controllers/Order/ShippingAddressController.cs
+++ HandWoven.Api/Controllers/Order/ShippingAddressController.cs
@@ -1,31 +1,31 @@
-using System.Security.Claims;
-using HandWoven.Api.DTOs.Order;
-using HandWoven.Api.Services.Order;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+// using System.Security.Claims;
+// using HandWoven.Api.DTOs.Order;
+// using HandWoven.Api.Services.Order;
+// using Microsoft.AspNetCore.Authorization;
+// using Microsoft.AspNetCore.Http;
+// using Microsoft.AspNetCore.Mvc;
 
-namespace HandWoven.Api.Controllers.Order
-{
-    [Route("api/user/shipping-address")]
-    [ApiController]
-    [Authorize]
-    public class ShippingAddressController : ControllerBase
-    {
-        private readonly IShippingAdService _shippingService;
+// namespace HandWoven.Api.Controllers.Order
+// {
+//     [Route("api/user/shipping-address")]
+//     [ApiController]
+//     [Authorize]
+//     public class ShippingAddressController : ControllerBase
+//     {
+//         private readonly IShippingAdService _shippingService;
 
-        public ShippingAddressController(IShippingAdService shippingAdService)
-        {
-            _shippingService = shippingAdService;
-        }
+//         public ShippingAddressController(IShippingAdService shippingAdService)
+//         {
+//             _shippingService = shippingAdService;
+//         }
 
-        [HttpPost("create")]
-        public async Task<IActionResult> Create(AddShippingAddressDto dto)
-        {
-            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
-            await _shippingService.AddShippingAddressAsync(userId, dto);
+//         [HttpPost("create")]
+//         public async Task<IActionResult> Create(AddShippingAddressDto dto)
+//         {
+//             var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+//             await _shippingService.AddShippingAddressAsync(userId, dto);
 
-            return Ok(new {message = "Shipping Data created"});
-        }
-    }
-}
+//             return Ok(new {message = "Shipping Data created"});
+//         }
+//     }
+// }
--- HandWoven.Api/DTOs/Order/CreateOrderResponse.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace HandWoven.Api.DTOs.Order;
-
-public class CreateOrderResponse
-{
-    public int OrderId { get; set; }
-    public string ClientSecret { get; set; } = null!;
-}
