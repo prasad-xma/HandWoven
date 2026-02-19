e4dc36afa6df4eea7de866888c5a8d6345366af7
Prasad Manamperi <prasadmanamperi130@gmail.com>
2026-02-19 10:50:58 +0530

    update: comment the file for testing

--- HandWoven.Api/Services/Order/IShippingAdService.cs
+++ HandWoven.Api/Services/Order/IShippingAdService.cs
@@ -1,9 +1,9 @@
-using System;
-using HandWoven.Api.DTOs.Order;
+// using System;
+// using HandWoven.Api.DTOs.Checkouts;
 
-namespace HandWoven.Api.Services.Order;
+// namespace HandWoven.Api.Services.Order;
 
-public interface IShippingAdService
-{
-    Task AddShippingAddressAsync(int userId, AddShippingAddressDto dto);
-}
+// public interface IShippingAdService
+// {
+//     Task AddShippingAddressAsync(int userId, AddShippingAddressDto dto);
+// }
