70aa29baeab070d3077fbdef364bac52da1abdab
Prasad Manamperi <prasadmanamperi130@gmail.com>
2026-02-19 16:57:15 +0530

    update: navigation button to shipping address page

--- HandWoven.Client/src/pages/components/Cart.jsx
+++ HandWoven.Client/src/pages/components/Cart.jsx
@@ -312,7 +312,7 @@ const Cart = () => {
 
                                 <button
                                     className="w-full bg-blue-600 text-white py-3 px-6 rounded-lg hover:bg-blue-700 transition-colors font-medium disabled:opacity-50"
-                                    onClick={() => navigate("/checkout")}
+                                    onClick={() => navigate("/shipping")}
                                     disabled={updating}
                                 >
                                     Proceed to Checkout
